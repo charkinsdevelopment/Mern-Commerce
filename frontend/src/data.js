a714ce72617e47085a298ab59fd77f7191b5f5f2
Cory Harkins <coryh@purecars.com>
2021-06-07 22:14:42 -0400

    set up react app

---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+let data = {
+    products: [
+        {
+            id: 1,
+            name: "Test Product",
+            category: "Shirt",
+            image: '/images/product-1.jpg',
+            price: 120,
+            rating: 4.5,
+            reviews: 10,
+            description: 'high quality test product'
+        },
+        {
+            id: 2,
+            name: "Test Product",
+            category: "Pants",
+            image: '/images/product-1.jpg',
+            price: 100,
+            rating: 5,
+            reviews: 2,
+            description: 'high quality test product'
+        },
+        {
+            id: 3,
+            name: "Test Product",
+            category: "Shoes",
+            image: '/images/product-1.jpg',
+            price: 80,
+            rating: 3.0,
+            reviews: 32,
+            description: 'high quality test product'
+        },
+        {
+            id: 4,
+            name: "Test Product",
+            category: "Shirt",
+            image: '/images/product-1.jpg',
+            price: 240,
+            rating: 4.9,
+            reviews: 12,
+            description: 'high quality test product'
+        },
+        {
+            id: 5,
+            name: "Test Product",
+            category: "Pants",
+            image: '/images/product-1.jpg',
+            price: 120,
+            rating: 4.5,
+            reviews: 10,
+            description: 'high quality test product'
+        },
+        {
+            id: 6,
+            name: "Test Product",
+            category: "Shoes",
+            image: '/images/product-1.jpg',
+            price: 139.99,
+            rating: 4.5,
+            reviews: 10,
+            description: 'high quality test product'
+        }
+    ]
+};
+
+export default data
