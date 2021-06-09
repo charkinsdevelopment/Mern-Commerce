60f2c29f8e45d869d80f803be53be60d1a591ebe
Cory Harkins <coryh@purecars.com>
2021-06-09 10:23:10 -0400

    node mon and server refactoring

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
+import data from "./data.js";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("server is ready");
 });
 
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
 app.listen(port, () => {
-  console.log("serve at http://localhost:" + port);
+  console.log(`serve at http://localhost:${port}`);
 });
